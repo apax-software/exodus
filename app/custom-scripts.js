58f316292d11b642efac6508bbd3ab29706d9317
Matt Smith <mtsmit2@gmail.com>
2017-05-05 12:50:59 -0400

    Add a sample chart.

--- app/custom-scripts.js
+++ app/custom-scripts.js
@@ -9,31 +9,6 @@ require(['dojo/topic'], function(topic) {
      * Custom Javascript to be executed when the application is ready goes here
      */
 
-     setTimeout(()=> {
-       var myChart = Highcharts.chart(app.data.sections[2]._medias[0]._domID, {
-           chart: {
-               type: 'bar'
-           },
-           title: {
-               text: ''
-           },
-           xAxis: {
-               categories: ['Apples', 'Bananas', 'Oranges']
-           },
-           yAxis: {
-               title: {
-                   text: 'Fruit eaten'
-               }
-           },
-           series: [{
-               name: 'Jane',
-               data: [1, 0, 4]
-           }, {
-               name: 'John',
-               data: [5, 7, 3]
-           }]
-       });
-     }, 0);
 
     //console.log('Cascade is ready');
   });
@@ -42,34 +17,34 @@ require(['dojo/topic'], function(topic) {
    * Custom Javascript to be executed when a section becomes active
    */
   topic.subscribe('story-navigated-section', function(cfg) {
-     console.log(cfg);
-     if (cfg.index === 2) {
-      console.log(cfg.data._blocks[3]._domID);
-      setTimeout(()=> {
-        var myChart = Highcharts.chart(app.data.sections[2]._medias[0]._domID, {
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: ''
-            },
-            xAxis: {
-                categories: ['Apples', 'Bananas', 'Oranges']
-            },
-            yAxis: {
-                title: {
-                    text: 'Fruit eaten'
-                }
-            },
-            series: [{
-                name: 'Jane',
-                data: [1, 0, 4]
-            }, {
-                name: 'John',
-                data: [5, 7, 3]
-            }]
-        });
-      }, 0);
+     // console.log(cfg);
+     // if (cfg.index === 2) {
+     //  console.log(cfg.data._blocks[3]._domID);
+     //  setTimeout(()=> {
+     //    var myChart = Highcharts.chart(app.data.sections[2]._medias[0]._domID, {
+     //        chart: {
+     //            type: 'bar'
+     //        },
+     //        title: {
+     //            text: ''
+     //        },
+     //        xAxis: {
+     //            categories: ['Apples', 'Bananas', 'Oranges']
+     //        },
+     //        yAxis: {
+     //            title: {
+     //                text: 'Fruit eaten'
+     //            }
+     //        },
+     //        series: [{
+     //            name: 'Jane',
+     //            data: [1, 0, 4]
+     //        }, {
+     //            name: 'John',
+     //            data: [5, 7, 3]
+     //        }]
+     //    });
+     //  }, 0);
      }
   });
 
