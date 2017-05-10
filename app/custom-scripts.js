155fc7669144189d2d8830462e2cea605f177c7e
Matt Smith <mtsmit2@gmail.com>
2017-05-10 09:02:37 -0400

    Remove setTimeout.

--- app/custom-scripts.js
+++ app/custom-scripts.js
@@ -17,34 +17,7 @@ require(['dojo/topic'], function(topic) {
    * Custom Javascript to be executed when a section becomes active
    */
   topic.subscribe('story-navigated-section', function(cfg) {
-     // console.log(cfg);
-     // if (cfg.index === 2) {
-     //  console.log(cfg.data._blocks[3]._domID);
-     //  setTimeout(()=> {
-     //    var myChart = Highcharts.chart(app.data.sections[2]._medias[0]._domID, {
-     //        chart: {
-     //            type: 'bar'
-     //        },
-     //        title: {
-     //            text: ''
-     //        },
-     //        xAxis: {
-     //            categories: ['Apples', 'Bananas', 'Oranges']
-     //        },
-     //        yAxis: {
-     //            title: {
-     //                text: 'Fruit eaten'
-     //            }
-     //        },
-     //        series: [{
-     //            name: 'Jane',
-     //            data: [1, 0, 4]
-     //        }, {
-     //            name: 'John',
-     //            data: [5, 7, 3]
-     //        }]
-     //    });
-     //  }, 0);
+
      }
   });
 
